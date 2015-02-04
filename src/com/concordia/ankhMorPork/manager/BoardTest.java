e42ec539eb2ee27374ecdc1f5c158a66164ed12b
taiseemdadwal <taiseemahmed1989@gmail.com>
2015-02-04 16:53:05 -0500

    Test cases classes
    junit testcases for almost all classes

---
+++ src/com/concordia/ankhMorPork/manager/BoardTest.java
@@ -0,0 +1,44 @@
+package com.concordia.ankhMorPork.manager;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BoardTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetBankMoney() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBankMoney() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPersonalityCardDetails() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPersonalityCardDetails() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNoOfPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNoOfPlayer() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/com/concordia/ankhMorPork/manager/CityAreaCardTest.java
@@ -0,0 +1,54 @@
+package com.concordia.ankhMorPork.manager;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CityAreaCardTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetIdentifier() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetIdentifier() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCost() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCost() {
+		fail("Not yet implemented");
+	}
+
+}
