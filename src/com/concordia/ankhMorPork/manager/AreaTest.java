4fdae3530b716923cdca000c12ebc2db69fc42a8
taiseemdadwal <taiseemahmed1989@gmail.com>
2015-02-04 17:47:37 -0500

    area test class testcases
    testcases of area class

--- src/com/concordia/ankhMorPork/manager/AreaTest.java
+++ src/com/concordia/ankhMorPork/manager/AreaTest.java
@@ -2,48 +2,75 @@ package com.concordia.ankhMorPork.manager;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.text.StyledEditorKit.BoldAction;
+
 import org.junit.Before;
 import org.junit.Test;
 
-public class AreaTest {
+import com.sun.org.apache.xalan.internal.xsltc.util.IntegerArray;
 
+public class AreaTest {
+	Boolean troubleMaker=false;
+	Boolean building=false;
+	Integer noOfDemon=0;
+	Integer noOfTroll=0;
+	Area area = new Area(troubleMaker, building, noOfDemon, noOfTroll);
 	@Before
 	public void setUp() throws Exception {
+		
+		List<String> colorOfMinion=new ArrayList<String>();
+		colorOfMinion.add("Red");
+		area.setColorOfMinion(colorOfMinion);
+		Integer identifier= 10;
+        area.setIdentifier(identifier);
+        @SuppressWarnings("unused")
+		Boolean troubleMaker= true;
+        area.setTroubleMaker(troubleMaker);
+        
 	}
 
-	@Test
+/*	@Test
 	public void testArea() {
 		fail("Not yet implemented");
 	}
-
+*/
 	@Test
 	public void testGetIdentifier() {
-		fail("Not yet implemented");
+		Integer expected=10;
+		assertEquals(expected, area.getIdentifier());
+		
 	}
 
-	@Test
+/*	@Test
 	public void testSetIdentifier() {
 		fail("Not yet implemented");
 	}
-
+*/
 	@Test
 	public void testGetColorOfMinion() {
-		fail("Not yet implemented");
+		List<String> expected=new ArrayList<String>();
+		expected.add("Red");
+		assertEquals(expected, area.getColorOfMinion());
+		
 	}
 
-	@Test
+/*	@Test
 	public void testSetColorOfMinion() {
 		fail("Not yet implemented");
 	}
-
+*/
 	@Test
 	public void testGetTroubleMaker() {
-		fail("Not yet implemented");
+	Boolean expected=true;
+	assertEquals(expected, area.getTroubleMaker());
 	}
 
-	@Test
+/*	@Test
 	public void testSetTroubleMaker() {
 		fail("Not yet implemented");
 	}
-
+*/
 }
