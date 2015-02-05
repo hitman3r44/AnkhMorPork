470e8d92f7c2769e0dc76f26966acb3a363e5a83
shezee611 <shezee611@yahoo.com>
2015-02-05 15:56:50 -0500

    comments on area class
    added comments on Area class

--- src/com/concordia/ankhMorPork/manager/Area.java
+++ src/com/concordia/ankhMorPork/manager/Area.java
@@ -15,6 +15,7 @@ public class Area {
 	Boolean building;
 	Integer noOfDemon;
 	Integer noOfTroll;
+	
 	/**
 	 * Parameterized constructor to initialize area details on instantiation.
 	 * @param troubleMaker : Boolean value if troubleMaker exists on an area.
@@ -31,39 +32,104 @@ public class Area {
 		this.colorOfMinion=new ArrayList<String>();
 	}
 	//setters and getters
+	
+	
+	/**
+	 * This functions is used to get the identifier of the area / area id.
+	 * @return It's returning the identifier
+	 */
 	public Integer getIdentifier() {
 		return identifier;
 	}
+	
+	
+	/**
+	 * This function is to set the area id we are passing at the time of area initialization
+	 * @param identifier : Area id is passed
+	 */
 	public void setIdentifier(Integer identifier) {
 		this.identifier = identifier;
 	}
+	
+	
+	/**
+	 * This function is used to get the list of the colors of minions.
+	 * @return It returns the list of colors of minions
+	 */
 	public List<String> getColorOfMinion() {
 		return colorOfMinion;
 	}
+	
+	/**
+	 * The function is used to set the color of minions 
+	 * @param colorOfMinion: A list of colors is passed to assigned to minions
+	 */
 	public void setColorOfMinion(List<String> colorOfMinion) {
 		this.colorOfMinion = colorOfMinion;
 	}
+	
+	/**
+	 * This function is called to check an area if trouble maker exists on a particular area 
+	 * @return Looking if a trouble make exist on an area
+	 */
 	public Boolean getTroubleMaker() {
 		return troubleMaker;
 	}
+	
+	/**
+	 * This function is used to set the trouble maker on an Area
+	 * @param troubleMaker: A boolean value is sent to set the area with trouble maker.
+	 */
 	public void setTroubleMaker(Boolean troubleMaker) {
 		this.troubleMaker = troubleMaker;
 	}
+	
+	/**
+	 * It's making sure if an area have building or not 
+	 * @return A boolean value is returned to confirm if a building exists on an area or not.
+	 */
 	public Boolean getBuilding() {
 		return building;
 	}
+	
+	
+	/**
+	 * This function is used to set the building on an Area
+	 * @param building: A boolean value is sent to set the area with building.
+	 */
+	
 	public void setBuilding(Boolean building) {
 		this.building = building;
 	}
+	
+	/**
+	 * This function is used to get number of demons on an area.
+	 * @return It returns the number of demon on an Area
+	 */
 	public Integer getNoOfDemon() {
 		return noOfDemon;
 	}
+	
+	/**
+	 * This function is used to set the number of demon on an area
+	 * @param noOfDemon Number value is passed to set the demon on an area
+	 */
 	public void setNoOfDemon(Integer noOfDemon) {
 		this.noOfDemon = noOfDemon;
 	}
+	
+	/**
+	 * This function is used to get number of trolls on an area.
+	 * @return It returns the number of trolls on an Area
+	 */
 	public Integer getNoOfTroll() {
 		return noOfTroll;
 	}
+	
+	/**
+	 * This function is used to set the number of trolls on an area
+	 * @param noOfTroll Number value is passed to set the troll on an area
+	 */
 	public void setNoOfTroll(Integer noOfTroll) {
 		this.noOfTroll = noOfTroll;
 	}
