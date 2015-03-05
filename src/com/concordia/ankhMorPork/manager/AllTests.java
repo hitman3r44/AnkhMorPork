47da6af0663fd0208148866681f5190a9d669b55
taiseemdadwal <taiseem.dadwal@gmail.com>
2015-03-05 01:01:04 -0500

    Test Suite
    Contains all the tests in one class

---
+++ src/com/concordia/ankhMorPork/manager/AllTests.java
@@ -0,0 +1,12 @@
+package com.concordia.ankhMorPork.manager;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AreaTest.class, BoardManagerTest.class, BoardTest.class,
+		CityAreaCardTest.class, PlayerTest.class })
+public class AllTests {
+
+}
