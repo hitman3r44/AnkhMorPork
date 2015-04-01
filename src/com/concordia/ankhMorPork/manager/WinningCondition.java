41451eaaed2f9602e5ce12c0524102fa971a1a60
hitman3r44 <hitman3r44@gmail.com>
2015-03-31 21:53:55 -0400

    Winning Condition Class and good git ignore file

---
+++ src/com/concordia/ankhMorPork/manager/WinningCondition.java
@@ -0,0 +1,52 @@
+package com.concordia.ankhMorPork.manager;
+
+/**
+ * @author elDiablo
+ * Mar 31, 2015
+ * 9:40:40 PM
+ * 2015
+ * @email: sumitsarkarbd@gmail.com
+ */
+public class WinningCondition {
+	
+	boolean lordVetinari(){
+		return false;
+	}
+	
+	
+	//All those have a same Winning condition
+	boolean lordSelachii(){
+		return false;
+	}
+	
+	boolean lordRust(){
+		return false;
+	}
+	
+	boolean lordDeWorde(){
+		return false;
+	}
+	
+	//Dragon King of Arms
+	boolean dragonKingOfArms(){
+		return false;
+	}
+	
+	//Chrysoprase
+	boolean chrysoprase(){
+		return false;
+	}
+	
+	//Commander Vimes
+	boolean commanderVimes(){
+		return false;
+	}
+	
+	//General Rules
+	boolean generalRules() {
+		return false;
+	}
+	
+	
+
+}
